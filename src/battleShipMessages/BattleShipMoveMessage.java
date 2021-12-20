5f8bb14905eafa26b2f38ce5a106f699fa389f1d
Nate <n.teku710@gmail.com>
2021-12-20 10:09:39 -0800

    Implemented MoveMessage Class

--- src/battleShipMessages/BattleShipMoveMessage.java
+++ src/battleShipMessages/BattleShipMoveMessage.java
@@ -1,5 +1,31 @@
 package battleShipMessages;
+import java.io.Serializable;
 
-public class BattleShipMoveMessage {
+public class BattleShipMoveMessage implements Serializable {
 
+	public static int RED = 1;
+	public static int BLACK = 2;
+	private static final long serialVersionUID = 1L;
+	private int row;
+	private int col;
+	private int status;
+	
+	
+	public BattleShipMoveMessage(int row, int col, int status) {
+		this.row = row;
+		this.col = col;
+		this.status = status;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+
+	public int getColumn() {
+		return col;
+	}
+
+	public int getStatus() {
+		return status;
+	}
 }
