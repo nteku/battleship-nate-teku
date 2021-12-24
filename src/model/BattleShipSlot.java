788a84ac444a57af65d53b10536a3bfe3f2acaf4
Nate <n.teku710@gmail.com>
2021-12-24 13:56:21 -0800

    Added all comments for the BattleShipSlot class

--- src/model/BattleShipSlot.java
+++ src/model/BattleShipSlot.java
@@ -1,28 +1,52 @@
 package model;
 
+/**
+ * This class represents the state of a slot in the Grid
+ * @author Nathan Teku
+ *
+ */
 public class BattleShipSlot {
 
+	// attributes include isGuessed and isMarkedBlack
 	private boolean isGuessed;
 	private boolean isMarkedBlack;
 	
+	/**
+	 * Construct initializes default values of attributes
+	 */
 	public BattleShipSlot () {
 		isGuessed = false;
 		isMarkedBlack = false;
 	}
 	
+	/**
+	 * Setter for guess value
+	 * @param guess value
+	 */
 	public void setGuessed(boolean guess) {
 		this.isGuessed = guess;
 	}
 	
+	/**
+	 * Getter for isGuessed value 
+	 * @return isGuessed value
+	 */
 	public boolean getIsGuessed() {
 		return isGuessed;
 	}
 	
-	
+	/**
+	 * Setter for isMarkedBlack value 
+	 * @param markedBlack value 
+	 */
 	public void setIsMarkedBlack(boolean markedBlack) {
 		this.isMarkedBlack = markedBlack;
 	}
 	
+	/**
+	 * Getter for isMarkedBlack value 
+	 * @return isMarkedBack value 
+	 */
 	public boolean getIsMarkedBlack() {
 		return isMarkedBlack;
 	}
