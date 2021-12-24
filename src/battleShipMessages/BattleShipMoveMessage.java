c4f453cf3ad59ac8a23db387c716bfd254658b2c
Nate <n.teku710@gmail.com>
2021-12-24 13:20:59 -0800

    Added all comments for the BattleShipMoveMessage class

--- src/battleShipMessages/BattleShipMoveMessage.java
+++ src/battleShipMessages/BattleShipMoveMessage.java
@@ -1,16 +1,27 @@
 package battleShipMessages;
 import java.io.Serializable;
 
+/**
+ * The purpose for this class is to send a Signal to the View of the correct coordinates for the slot
+ * @author Nathan Teku
+ *
+ */
 public class BattleShipMoveMessage implements Serializable {
 
-	public static int RED = 1;
-	public static int BLACK = 2;
+	// attributes include the serialID, row,col, status and the current player
 	private static final long serialVersionUID = 1L;
 	private int row;
 	private int col;
 	private int status;
 	private int currentPlayer;
 	
+	/**
+	 * Constructor is used to send the signal to the View from the Model
+	 * @param row , that was clicked
+	 * @param col , that was clicked
+	 * @param status , state of the slot
+	 * @param currentPlayer , which player is making the move 
+	 */
 	public BattleShipMoveMessage(int row, int col, int status, int currentPlayer) {
 		this.row = row;
 		this.col = col;
@@ -18,17 +29,33 @@ public class BattleShipMoveMessage implements Serializable {
 		this.currentPlayer = currentPlayer;
 	}
 	
+	/**
+	 * This function returns the value of the current player 
+	 * @return the currentPlayer value
+	 */
 	public int getCurrentPlayer() {
 		return currentPlayer;
 	}
+	/**
+	 * This function returns the value of the row
+	 * @return the row value 
+	 */
 	public int getRow() {
 		return row;
 	}
 
+	/**
+	 * This function returns the value of the column
+	 * @return the column value 
+	 */
 	public int getColumn() {
 		return col;
 	}
 
+	/**
+	 * This function returns the value of the status
+	 * @return the status value 
+	 */
 	public int getStatus() {
 		return status;
 	}
