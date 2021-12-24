7c6969e7c264c37592d551e4eb6f83f88297a5b1
Nate <n.teku710@gmail.com>
2021-12-24 14:00:35 -0800

    Added all comments for the BattleShip class

--- src/model/BattleShip.java
+++ src/model/BattleShip.java
@@ -2,17 +2,34 @@ package model;
 
 import java.io.Serializable;
 
+
+/**
+ * This class is a message that holds a single BattleShip
+ * @author Nathan Teku
+ *
+ */
+@SuppressWarnings("serial")
 public class BattleShip implements Serializable {
 
+	// row attributes
 	private int row1;
 	private int row2;
 	private int row3;
 	
-	
+	// column attributes
 	private int col1;
 	private int col2;
 	private int col3;
 	
+	/**
+	 * Constructor that holds the coordinates of a single ship
+	 * @param row1 , 1st row
+	 * @param col1 , 1st column
+	 * @param row2 , 2nd row
+	 * @param col2 , 2nd column
+	 * @param row3 , 3rd row
+	 * @param col3 , 3rd column
+	 */
 	public BattleShip (int row1,int col1, int row2, int col2, int row3, int col3) {
 		
 		this.row1 = row1;
@@ -26,27 +43,50 @@ public class BattleShip implements Serializable {
 	}
 	
 	
-	
+	/**
+	 * Getter for 1st row
+	 * @return row1 value 
+	 */
 	public int getRow1() {
 		return row1;
 	}
 	
+	/**
+	 * Getter for 2nd row
+	 * @return row2 value 
+	 */
 	public int getRow2() {
 		return row2;
 	}
 	
+	/**
+	 * Getter for 3rd row
+	 * @return row3 value 
+	 */
 	public int getRow3() {
 		return row3;
 	}
 	
+	/**
+	 * Getter for 1st column
+	 * @return col1 value 
+	 */
 	public int getCol1() {
 		return col1;
 	}
 	
+	/**
+	 * Getter for 2nd column
+	 * @return col2 value 
+	 */
 	public int getCol2() {
 		return col2;
 	}
 	
+	/**
+	 * Getter for 3rd column
+	 * @return col3 value 
+	 */
 	public int getCol3() {
 		return col3;
 	}
